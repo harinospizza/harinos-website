07e0094d486f7ed34a764f35d877011a5df2e00a
harinospizza <anubharatdwaj1@gmail.com>
2026-01-25 23:24:39 +0530

    Update MenuSection.tsx

--- components/MenuSection.tsx
+++ components/MenuSection.tsx
@@ -6,7 +6,6 @@ interface MenuSectionProps {
   items: MenuItem[];
   onAddToCart: (item: MenuItem, selectedSize?: string) => void;
   activeOffers: Offer[];
-  selectedCategory: string;
 }
 
 const MenuCard: React.FC<{ 
@@ -114,14 +113,10 @@ const MenuCard: React.FC<{
   );
 };
 
-const MenuSection: React.FC<MenuSectionProps> = ({ items, onAddToCart, activeOffers, selectedCategory }) => {
-const filteredItems = items.filter(
-  item => item.category === selectedCategory
-);
-
+const MenuSection: React.FC<MenuSectionProps> = ({ items, onAddToCart, activeOffers }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {filteredItems.map((item) => (
+    <div>
+      {items.map((item) => (
         <MenuCard 
           key={item.id} 
           item={item} 
