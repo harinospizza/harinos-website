fc890703e84b3e2b8a8c623a4b077ece1a24b035
harinospizza <anubharatdwaj1@gmail.com>
2026-04-05 23:10:13 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { checkForUpdate } from './updateChecker';
-
-checkForUpdate();
+import { registerFreshRuntime } from './services/runtime';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -16,3 +14,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+void registerFreshRuntime();
